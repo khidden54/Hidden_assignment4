aee8de3f3692395080bc022dd2d2c6517c36f2f3
khidden54 <khidden54@gmail.com>
2020-03-29 21:27:14 -0400

    Assignment 4 submission

---
+++ Assignment_4/src/hidden_problem2/Application.java
@@ -0,0 +1,11 @@
+package hidden_problem2;
+
+public class Application {
+
+	public static void main(String[] args) {
+		DuplicateCounter duplicateCounter = new DuplicateCounter();
+		duplicateCounter.count("problem2.txt");
+		duplicateCounter.write("unique_word_counts.txt");
+	}
+
+}
